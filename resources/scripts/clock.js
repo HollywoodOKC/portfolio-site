9c907ebd26ad0862c491a6128b7ce47ebfcb0ab2
HollywoodOKC <T_Lane11@yahoo.com>
2023-03-20 16:29:31 -0500

    Working on designs.

--- resources/scripts/clock.js
+++ resources/scripts/clock.js
@@ -5,8 +5,17 @@ const theTime = () => {
     const hours = t.getHours();
     const mins = t.getMinutes();
     const secs = t.getSeconds();
-    const  myTime = `${hours}:${mins}:${secs}`;
+    const myTime = `${hours}:${mins}:${secs}`;
     time.innerHTML = myTime;
 }
 
 setInterval(theTime, 1000);
+
+const myFunction = () => {
+    let x = document.getElementById("myTopNav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
