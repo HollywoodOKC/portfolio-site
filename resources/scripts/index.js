7e7de7b5192c2425e2df9ec662d8559433136ef7
HollywoodOKC <T_Lane11@yahoo.com>
2023-05-08 19:56:28 -0500

    Added button color.

---
+++ resources/scripts/index.js
@@ -0,0 +1,27 @@
+const button1 = document.getElementById('button1');
+const button2 = document.getElementById('button2');
+
+const changeBtn1 = () => {
+    button1.style.backgroundColor = 'hsl(356, 100%, 67.5%)';
+    button1.style.color = 'hsl(0, 0%, 100%)';
+}
+
+const changeBtn2 = () => {
+    button2.style.backgroundColor = 'hsl(356, 100%, 67.5%)';
+    button2.style.color = 'hsl(0, 0%, 100%)';
+}
+
+const changeBtn1Back = () => {
+    button1.style.backgroundColor = 'hsl(0, 0%, 100%)';
+    button1.style.color = 'hsl(0, 0%, 0%)';
+}
+
+const changeBtn2Back = () => {
+    button2.style.backgroundColor = 'hsl(0, 0%, 100%)';
+    button2.style.color = 'hsl(0, 0%, 0%)';
+}
+
+button1.addEventListener('mouseover', changeBtn1);
+button1.addEventListener('mouseout', changeBtn1Back);
+button2.addEventListener('mouseover', changeBtn2);
+button2.addEventListener('mouseout', changeBtn2Back);
